d7f0512df4564a739340637366940cfe697aa519
Vishnu Krishnakumar <83391274+vishnukkrishna@users.noreply.github.com>
2024-02-12 16:00:53 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import { Button } from "@material-tailwind/react";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button>Button</Button>
+    </>
+  );
+}
+
+export default App;
